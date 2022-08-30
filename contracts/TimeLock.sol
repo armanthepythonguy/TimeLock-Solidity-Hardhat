627550dfd22012df94704639fd06f70cd300157d
Arman Aurobindo <arman8895884469@.com>
2022-08-30 18:59:10 +0530

    added ERC721 token support

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.0;
 
 import "./Token.sol";
 
+interface IERC721 {
+    function transferFrom(address _from, address _to, uint256 _id) external;
+}
+
 contract TimeLock{
     
     struct LockInstant {
@@ -23,18 +27,32 @@ contract TimeLock{
 
     uint256 currentIndex = 0;
 
-    function createLock(address _contractAddress, uint256 _amount, uint _duration) public {
+    function createLockERC20(address _contractAddress, uint256 _amount, uint _duration) public {
         Token token = Token(_contractAddress);
         token.transferFrom(msg.sender, address(this), _amount);
         lockInstants.push(LockInstant(msg.sender, _duration, block.timestamp, _amount, _contractAddress));
         currentIndex++;
     }
 
-    function breakLock(uint256 _index) public {
+    function breakLockERC20(uint256 _index) public {
         require(msg.sender == lockInstants[_index].owner, "Only owner can open the lock");
         require(block.timestamp > lockInstants[_index].starttime + lockInstants[_index].duration, "Lock time not completed");
         Token token = Token(lockInstants[_index].contractAddress);
         token.transfer(msg.sender, lockInstants[_index].amount);
+    }
+
+    // In ERC721 contracts instead of passing out the amount we should pass the tokenId from the frontend
+
+    function createLockERC721(address _contractAddress, uint256 _amount, uint _duration) public {
+        IERC721(_contractAddress).transferFrom(address(msg.sender), address(this), _amount);
+        lockInstants.push(LockInstant(msg.sender, _duration, block.timestamp, _amount, _contractAddress));
+        currentIndex++;
+    }
+
+    function breakLockERC721(uint256 _index) public {
+        require(msg.sender == lockInstants[_index].owner, "Only owner can open the lock");
+        require(block.timestamp > lockInstants[_index].starttime + lockInstants[_index].duration, "Lock time not completed");
+        IERC721(lockInstants[_index].contractAddress).transferFrom(address(this), lockInstants[_index].owner, lockInstants[_index].amount);
     }
 
     function getLock(uint256 _index) public view returns(LockInstant memory){
--- test/TimeLock.js
+++ test/TimeLock.js
@@ -5,46 +5,68 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 const ether = tokens
 
 describe("Time Lock Testing", ()=>{
-    let deployer, token1Contract, token1, timeLockContract, timeLock, token2Contract, token2, tx;
+    let deployer, token1Contract, token1, timeLockContract, timeLock, tokenERC721Contract, tokenERC721, tx;
     beforeEach(async()=>{
         let accounts = await ethers.getSigners()
         deployer = accounts[0]
+        user = accounts[1]
         token1Contract = await ethers.getContractFactory("Token1")
         token1 = await token1Contract.deploy(1000)
-        token2Contract = await ethers.getContractFactory("Token2")
-        token2 = await token2Contract.deploy(1000)
+        tokenERC721Contract = await ethers.getContractFactory("TokenERC721")
+        tokenERC721 = await tokenERC721Contract.deploy()
+        for(let i=0; i<10; i++){
+            await tokenERC721.safeMint(deployer.address)
+        }
         timeLockContract = await ethers.getContractFactory("TimeLock")
         timeLock = await timeLockContract.deploy()
         tx = await token1.approve(timeLock.address,200)
+        for(let i=0; i<10; i++){
+            await tokenERC721.approve(timeLock.address, i)
+        }
         await tx.wait()
     })
     describe("Testing the working of the lock", async()=>{
         it("Trying with one token", async()=>{
 
             // Checking the balance of the deployer's and contract before creating timelock
-            console.log(`Deployer's address before creating timelocks is :-  ${await token1.balanceOf(deployer.address)}`)
-            console.log(`Contracts balance before creating timelock is :- ${await token1.balanceOf(timeLock.address)}`)
+            console.log(`Deployer's address before creating an ERC20 timelocks is :-  ${await token1.balanceOf(deployer.address)}`)
+            console.log(`Contracts balance before creating an ERC20 timelock is :- ${await token1.balanceOf(timeLock.address)}`)
             // Creating 10 timelocks each of 10 tokens and 1sec of timelock
             for(let i=0; i<10; i++){
-                await timeLock.createLock(token1.address, 10, 1)
+                await timeLock.createLockERC20(token1.address, 10, 1)
             }
             // Checking the balance of the deployer's and contract after creating timelock
-            console.log(`Deployer's address after creating timelocks is :-  ${await token1.balanceOf(deployer.address)}`)
-            console.log(`Contracts balance after creating timelock is :- ${await token1.balanceOf(timeLock.address)}`)
+            console.log(`Deployer's address after creating an ERC20 timelocks is :-  ${await token1.balanceOf(deployer.address)}`)
+            console.log(`Contracts balance after creating an ERC20 timelock is :- ${await token1.balanceOf(timeLock.address)}`)
 
             // Breaking the 1st timelock 
-            await timeLock.breakLock(0)
+            await timeLock.breakLockERC20(0)
 
             // Checking the balance of the deployer's and contract after breaking timelock
-            console.log(`Deployer's address after breaking timelocks is :-  ${await token1.balanceOf(deployer.address)}`)
-            console.log(`Contracts balance after breaking timelock is :- ${await token1.balanceOf(timeLock.address)}`)
+            console.log(`Deployer's address after breaking an ERC20 timelocks is :-  ${await token1.balanceOf(deployer.address)}`)
+            console.log(`Contracts balance after breaking an ERC20 timelock is :- ${await token1.balanceOf(timeLock.address)}`)
+
+            // Checking the balance of the deployer's and contract before creating timelock
+            console.log(`Deployer's address before creating an ERC721 timelocks is :-  ${await tokenERC721.balanceOf(deployer.address)}`)
+            console.log(`Contracts balance before creating an ERC721 timelock is :- ${await tokenERC721.balanceOf(timeLock.address)}`)
+
+            // Creating 10 timelocks each of 10 tokens and 1sec of timelock
+            for(let i=0; i<10; i++){
+                await timeLock.createLockERC721(tokenERC721.address, i, 1)
+            }
+
+            // Checking the balance of the deployer's and contract after creating timelock
+            console.log(`Deployer's address after creating an ERC721 timelocks is :-  ${await tokenERC721.balanceOf(deployer.address)}`)
+            console.log(`Contracts balance after creating an ERC721 timelock is :- ${await tokenERC721.balanceOf(timeLock.address)}`)
+
+            // Breaking the 1st timelock 
+            await timeLock.breakLockERC721(10)
+            // Checking the balance of the deployer's and contract after breaking timelock
+            console.log(`Deployer's address after breaking an ERC721 timelocks is :-  ${await tokenERC721.balanceOf(deployer.address)}`)
+            console.log(`Contracts balance after breaking an ERC721 timelock is :- ${await tokenERC721.balanceOf(timeLock.address)}`)
 
         })
     })
